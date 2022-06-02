4009810d53cb6f2fb01e9762c2b83fbc01ca7e26
MrSobodakho <Mr.Sobodakho@proton.me>
2022-06-02 18:46:32 +0300

    corrected the code in file

--- SergeiSobodakho.homework.lesson19.py
+++ SergeiSobodakho.homework.lesson19.py
@@ -1,18 +1,12 @@
 '''Hometask_13_1
 Из полученного списка чисел создайте список с суммами этих чисел, отсортированными по возрастанию.
 
-while True:
-    try:
-        user_in = [int(i) for i in input("Please enter numbers: ").split()]
-    except ValueError:
-        print("You entered an invalid character! Please re-enter.")
-    else:
-        break
+user_in = [i for i in input("Please enter numbers: ").split()]
 
 def sum_digits(add_list):
     sum_list = []
     for i in add_list:
-        sum_list.append(sum(map(int, str(i))))
+        sum_list.append(sum(map(int, i)))
     return sorted(sum_list)
 
 print(f"Result: {sum_digits(user_in)}")'''
@@ -20,7 +14,7 @@ print(f"Result: {sum_digits(user_in)}")'''
 '''Hometask_13_2
 Напишите функцию f(x), которая возвращает значение следующей функции, определённой на всей числовой прямой:
 
-while True:
+while True: #для отсечения ошибок с символами без перезапуска программы
     try:
         user_in = float(input("Enter one number: "))
     except ValueError:
@@ -29,19 +23,12 @@ while True:
         break
 
 def calc_function (x):
-    def function_1 (numbers):
-        return 1 - (numbers + 2)**2
-    def function_2 (numbers):
-        return - (numbers / 2)
-    def function_3 (numbers):
-        return (numbers - 2)**2 + 1
-
     if x <= -2:
-        return function_1(x)
+        return 1 - (x + 2)**2
     elif x > -2 and x <= 2:
-        return function_2(x)
+        return - (x / 2)
     else:
-        return function_3(x)
+        return (x - 2)**2 + 1
 
 print(f"Answer: {calc_function(user_in)}")'''
 
@@ -49,7 +36,7 @@ print(f"Answer: {calc_function(user_in)}")'''
 Напишите функцию которая принимает на вход список целых чисел, удаляет из него все нечётные значения, а 
 чётные нацело делит на два.
 
-while True:
+while True: #для отсечения ошибок с символами без перезапуска программы
     try:
         user_in = [int(i) for i in input("Please enter numbers: ").split()]
     except ValueError:
@@ -60,4 +47,4 @@ while True:
 def list_editor (inc_list):
     return [int(i / 2) for i in user_in if not i % 2]
 
-print(list_editor(user_in))'''
+print(f"Result: {list_editor(user_in)}")'''
